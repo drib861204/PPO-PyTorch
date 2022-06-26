19a86e80b0c2b3c9d53a11181c429bbd80ea4ad2
drib861204 <56364972+drib861204@users.noreply.github.com>
2022-06-27 02:13:21 +0800

    0627 8-10 done: |q1|>5 deg

--- try.sh
+++ try.sh
@@ -1,91 +1,91 @@
 #!/bin/sh
 
-echo trial 3 seed 0
-python3 train.py -trial 3 -seed 0 -frames 200000 -w_q1 1
-echo trial 3 seed 1
-python3 train.py -trial 3 -seed 1 -frames 200000 -w_q1 1
-echo trial 3 seed 2
-python3 train.py -trial 3 -seed 2 -frames 200000 -w_q1 1
-echo trial 3 seed 3
-python3 train.py -trial 3 -seed 3 -frames 200000 -w_q1 1
-echo trial 3 seed 4
-python3 train.py -trial 3 -seed 4 -frames 200000 -w_q1 1
+echo trial 8 seed 0
+python3 train.py -trial 8 -seed 0 -frames 200000 -w_q1 10
+echo trial 8 seed 1
+python3 train.py -trial 8 -seed 1 -frames 200000 -w_q1 10
+echo trial 8 seed 2
+python3 train.py -trial 8 -seed 2 -frames 200000 -w_q1 10
+echo trial 8 seed 3
+python3 train.py -trial 8 -seed 3 -frames 200000 -w_q1 10
+echo trial 8 seed 4
+python3 train.py -trial 8 -seed 4 -frames 200000 -w_q1 10
 
-echo trial 4 seed 0
-python3 train.py -trial 4 -seed 0 -frames 200000 -w_q1 5
-echo trial 4 seed 1
-python3 train.py -trial 4 -seed 1 -frames 200000 -w_q1 5
-echo trial 4 seed 2
-python3 train.py -trial 4 -seed 2 -frames 200000 -w_q1 5
-echo trial 4 seed 3
-python3 train.py -trial 4 -seed 3 -frames 200000 -w_q1 5
-echo trial 4 seed 4
-python3 train.py -trial 4 -seed 4 -frames 200000 -w_q1 5
+echo trial 9 seed 0
+python3 train.py -trial 9 -seed 0 -frames 200000 -w_q1 100
+echo trial 9 seed 1
+python3 train.py -trial 9 -seed 1 -frames 200000 -w_q1 100
+echo trial 9 seed 2
+python3 train.py -trial 9 -seed 2 -frames 200000 -w_q1 100
+echo trial 9 seed 3
+python3 train.py -trial 9 -seed 3 -frames 200000 -w_q1 100
+echo trial 9 seed 4
+python3 train.py -trial 9 -seed 4 -frames 200000 -w_q1 100
 
-echo trial 5 seed 0
-python3 train.py -trial 5 -seed 0 -frames 200000 -w_q1 10
-echo trial 5 seed 1
-python3 train.py -trial 5 -seed 1 -frames 200000 -w_q1 10
-echo trial 5 seed 2
-python3 train.py -trial 5 -seed 2 -frames 200000 -w_q1 10
-echo trial 5 seed 3
-python3 train.py -trial 5 -seed 3 -frames 200000 -w_q1 10
-echo trial 5 seed 4
-python3 train.py -trial 5 -seed 4 -frames 200000 -w_q1 10
+echo trial 10 seed 0
+python3 train.py -trial 10 -seed 0 -frames 200000 -w_q1 1000
+echo trial 10 seed 1
+python3 train.py -trial 10 -seed 1 -frames 200000 -w_q1 1000
+echo trial 10 seed 2
+python3 train.py -trial 10 -seed 2 -frames 200000 -w_q1 1000
+echo trial 10 seed 3
+python3 train.py -trial 10 -seed 3 -frames 200000 -w_q1 1000
+echo trial 10 seed 4
+python3 train.py -trial 10 -seed 4 -frames 200000 -w_q1 1000
 
-echo trial 6 seed 0
-python3 train.py -trial 6 -seed 0 -frames 200000 -w_q1 100
-echo trial 6 seed 1
-python3 train.py -trial 6 -seed 1 -frames 200000 -w_q1 100
-echo trial 6 seed 2
-python3 train.py -trial 6 -seed 2 -frames 200000 -w_q1 100
-echo trial 6 seed 3
-python3 train.py -trial 6 -seed 3 -frames 200000 -w_q1 100
-echo trial 6 seed 4
-python3 train.py -trial 6 -seed 4 -frames 200000 -w_q1 100
+#echo trial 11 seed 0
+#python3 train.py -trial 11 -seed 0 -frames 200000 -w_q1 100
+#echo trial 11 seed 1
+#python3 train.py -trial 11 -seed 1 -frames 200000 -w_q1 100
+#echo trial 11 seed 2
+#python3 train.py -trial 11 -seed 2 -frames 200000 -w_q1 100
+#echo trial 11 seed 3
+#python3 train.py -trial 11 -seed 3 -frames 200000 -w_q1 100
+#echo trial 11 seed 4
+#python3 train.py -trial 11 -seed 4 -frames 200000 -w_q1 100
 
-echo trial 7 seed 0
-python3 train.py -trial 7 -seed 0 -frames 200000 -w_q1 1000
-echo trial 7 seed 1
-python3 train.py -trial 7 -seed 1 -frames 200000 -w_q1 1000
-echo trial 7 seed 2
-python3 train.py -trial 7 -seed 2 -frames 200000 -w_q1 1000
-echo trial 7 seed 3
-python3 train.py -trial 7 -seed 3 -frames 200000 -w_q1 1000
-echo trial 7 seed 4
-python3 train.py -trial 7 -seed 4 -frames 200000 -w_q1 1000
+#echo trial 12 seed 0
+#python3 train.py -trial 12 -seed 0 -frames 200000 -w_q1 1000
+#echo trial 12 seed 1
+#python3 train.py -trial 12 -seed 1 -frames 200000 -w_q1 1000
+#echo trial 12 seed 2
+#python3 train.py -trial 12 -seed 2 -frames 200000 -w_q1 1000
+#echo trial 12 seed 3
+#python3 train.py -trial 12 -seed 3 -frames 200000 -w_q1 1000
+#echo trial 12 seed 4
+#python3 train.py -trial 12 -seed 4 -frames 200000 -w_q1 1000
 
-python3 plot_graph.py --trial 3
-python3 test.py -trial 3 -seed 0 -w_q1 1
-python3 test.py -trial 3 -seed 1 -w_q1 1
-python3 test.py -trial 3 -seed 2 -w_q1 1
-python3 test.py -trial 3 -seed 3 -w_q1 1
-python3 test.py -trial 3 -seed 4 -w_q1 1
+python3 plot_graph.py --trial 8
+python3 test.py -trial 8 -seed 0 -w_q1 10
+python3 test.py -trial 8 -seed 1 -w_q1 10
+python3 test.py -trial 8 -seed 2 -w_q1 10
+python3 test.py -trial 8 -seed 3 -w_q1 10
+python3 test.py -trial 8 -seed 4 -w_q1 10
 
-python3 plot_graph.py --trial 4
-python3 test.py -trial 4 -seed 0 -w_q1 5
-python3 test.py -trial 4 -seed 1 -w_q1 5
-python3 test.py -trial 4 -seed 2 -w_q1 5
-python3 test.py -trial 4 -seed 3 -w_q1 5
-python3 test.py -trial 4 -seed 4 -w_q1 5
+python3 plot_graph.py --trial 9
+python3 test.py -trial 9 -seed 0 -w_q1 100
+python3 test.py -trial 9 -seed 1 -w_q1 100
+python3 test.py -trial 9 -seed 2 -w_q1 100
+python3 test.py -trial 9 -seed 3 -w_q1 100
+python3 test.py -trial 9 -seed 4 -w_q1 100
 
-python3 plot_graph.py --trial 5
-python3 test.py -trial 5 -seed 0 -w_q1 10
-python3 test.py -trial 5 -seed 1 -w_q1 10
-python3 test.py -trial 5 -seed 2 -w_q1 10
-python3 test.py -trial 5 -seed 3 -w_q1 10
-python3 test.py -trial 5 -seed 4 -w_q1 10
+python3 plot_graph.py --trial 10
+python3 test.py -trial 10 -seed 0 -w_q1 1000
+python3 test.py -trial 10 -seed 1 -w_q1 1000
+python3 test.py -trial 10 -seed 2 -w_q1 1000
+python3 test.py -trial 10 -seed 3 -w_q1 1000
+python3 test.py -trial 10 -seed 4 -w_q1 1000
 
-python3 plot_graph.py --trial 6
-python3 test.py -trial 6 -seed 0 -w_q1 100
-python3 test.py -trial 6 -seed 1 -w_q1 100
-python3 test.py -trial 6 -seed 2 -w_q1 100
-python3 test.py -trial 6 -seed 3 -w_q1 100
-python3 test.py -trial 6 -seed 4 -w_q1 100
+#python3 plot_graph.py --trial 11
+#python3 test.py -trial 11 -seed 0 -w_q1 100
+#python3 test.py -trial 11 -seed 1 -w_q1 100
+#python3 test.py -trial 11 -seed 2 -w_q1 100
+#python3 test.py -trial 11 -seed 3 -w_q1 100
+#python3 test.py -trial 11 -seed 4 -w_q1 100
 
-python3 plot_graph.py --trial 7
-python3 test.py -trial 7 -seed 0 -w_q1 1000
-python3 test.py -trial 7 -seed 1 -w_q1 1000
-python3 test.py -trial 7 -seed 2 -w_q1 1000
-python3 test.py -trial 7 -seed 3 -w_q1 1000
-python3 test.py -trial 7 -seed 4 -w_q1 1000
+#python3 plot_graph.py --trial 12
+#python3 test.py -trial 12 -seed 0 -w_q1 1000
+#python3 test.py -trial 12 -seed 1 -w_q1 1000
+#python3 test.py -trial 12 -seed 2 -w_q1 1000
+#python3 test.py -trial 12 -seed 3 -w_q1 1000
+#python3 test.py -trial 12 -seed 4 -w_q1 1000
